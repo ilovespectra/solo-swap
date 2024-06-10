97374b2b14d46816331bdb68643af1c778b4a15a
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-06-10 17:16:51 -0400

    Create PercentageContext.tsx

---
+++ src/PercentageContext.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface PercentageContextProps {
+  percentage: number;
+  setPercentage: (value: number) => void;
+}
+
+const PercentageContext = createContext<PercentageContextProps | undefined>(undefined);
+
+export const PercentageProvider = ({ children }: { children: ReactNode }) => {
+  const [percentage, setPercentage] = useState<number>(0);
+
+  return (
+    <PercentageContext.Provider value={{ percentage, setPercentage }}>
+      {children}
+    </PercentageContext.Provider>
+  );
+};
+
+export const usePercentage = () => {
+  const context = useContext(PercentageContext);
+  if (!context) {
+    throw new Error('usePercentage must be used within a PercentageProvider');
+  }
+  return context;
+};

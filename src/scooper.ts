dedb42393ca37b532d92745e2ed657a33a2ef16a
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-07-26 06:30:15 -0600

    Update scooper.ts
    add quicknode env variable

--- src/scooper.ts
+++ src/scooper.ts
@@ -479,13 +479,13 @@ async function findQuotes(
  * @returns [instance of Jupiter API, map of known token types by mint address]
  */
 async function loadJupyterApi(): Promise<[DefaultApi, { [id: string]: TokenInfo }]> {
-  // const ENDPOINT = "your-quicknode-pro-plan-endpoint";
-  // const CONFIG = {
-  //   basePath: ENDPOINT,
-  // };
-  // let quoteApi = createJupiterApiClient(CONFIG);
+  const ENDPOINT = process.env.QUICKNODE_API;
+  const CONFIG = {
+    basePath: ENDPOINT,
+  };
+  let quoteApi = createJupiterApiClient(CONFIG);
 
-  let quoteApi = createJupiterApiClient();
+  // let quoteApi = createJupiterApiClient();
   const allTokens = await fetch("https://token.jup.ag/all");
   const allList = await allTokens.json();
   const tokenMap: { [id: string]: TokenInfo } = {};

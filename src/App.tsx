d1faf2c49635f43e77923e67a3c26fadcd5c1ae8
TheRealAncientHuman <ancient@hydralaunchpad.com>
2024-07-29 00:43:10 +0800

    feature: slippage, priority fee

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   useConnection,
   useWallet,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import {
   UnsafeBurnerWalletAdapter,
   PhantomWalletAdapter,
@@ -24,7 +21,7 @@ import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-route
 import AssetList from "./components/AssetList";
 import Info from "./components/Info";
 import Header from "./components/Header";
-import { PercentageProvider } from './PercentageContext';
+import { PercentageProvider } from "./PercentageContext";
 
 require("./App.css");
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -75,22 +72,32 @@ const Home: FC = () => {
   const navigate = useNavigate();
 
   const handleEnterSolo = () => {
-    navigate('/assets');
+    navigate("/assets");
   };
 
   return (
-    <div className="p-4 sm:p-8 md:p-16 lg:p-24 min-h-screen relative" style={{ backgroundImage: `url('https://github.com/ilovespectra/solo-explorer/blob/main/src/lib/assets/helius/bg.png?raw=true')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <Header />
-      <Info />
-      <div className="text-center mt-10">
-        <button
-          onClick={handleEnterSolo}
-          className="enter-solo-button bg-black border border-white text-white py-2 px-4 rounded-xl"
-        >
-          enter solo
-        </button>
+    <>
+      <div
+        className="fixed inset-0 -z-10"
+        style={{
+          backgroundImage: `url('https://github.com/ilovespectra/solo-explorer/blob/main/src/lib/assets/helius/bg.png?raw=true')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="p-4 sm:p-8 md:p-16 lg:p-24 min-h-screen relative">
+        <Header />
+        <Info />
+        <div className="text-center mt-10">
+          <button
+            onClick={handleEnterSolo}
+            className="enter-solo-button bg-black border border-white text-white py-2 px-4 rounded-xl"
+          >
+            enter solo
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -99,19 +106,34 @@ const AssetListPage: FC = () => {
   const wallet = useWallet();
 
   return (
-    <div className="p-4 sm:p-8 md:p-16 lg:p-24 min-h-screen relative" style={{ backgroundImage: `url('https://github.com/ilovespectra/solo-explorer/blob/main/src/lib/assets/helius/bg.png?raw=true')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <Header />
-      {!wallet || !connection || !wallet.publicKey ? (
-        <>
-          <Info />
-          <div className="lowercase text-white text-center pt-4 font-bold text-2xl italic h-[30vh] flex items-center justify-center relative z-40">
-            connect your wallet to create a pro rata swap
-          </div>
-        </>
-      ) : (
-        <AssetList />
-      )}
-      <img src={`/images/bonk_logo_transparent.png`} width={500} className="absolute bottom-0 left-0" alt="solo logo" />
-    </div>
+    <>
+      <div
+        className="fixed inset-0 -z-10"
+        style={{
+          backgroundImage: `url('https://github.com/ilovespectra/solo-explorer/blob/main/src/lib/assets/helius/bg.png?raw=true')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="p-4 sm:p-8 md:p-16 lg:p-24 min-h-screen relative">
+        <Header />
+        {!wallet || !connection || !wallet.publicKey ? (
+          <>
+            <Info />
+            <div className="lowercase text-white text-center pt-4 font-bold text-2xl italic h-[30vh] flex items-center justify-center relative z-40">
+              connect your wallet to create a pro rata swap
+            </div>
+          </>
+        ) : (
+          <AssetList />
+        )}
+        <img
+          src={`/images/bonk_logo_transparent.png`}
+          width={500}
+          className="absolute bottom-0 left-0"
+          alt="solo logo"
+        />
+      </div>
+    </>
   );
 };
